7bb1ed5fa167d3eb552eef5068a30e2a7150cad4
Auriga-Xebec <97719033+Auriga-Xebec@users.noreply.github.com>
2022-02-19 22:52:56 +0800

    Remaining work

---
+++ mega_menu.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+bash new_password_check.sh
+con=$?
+
+while true; do
+    if [ "$con" = 0 ]; then
+        echo "(1) Folder Creator
+(2) Copy folder
+(3) Set Password
+(4) Calculator
+(5) Create week folders
+(6) Check file names
+(7) Download a file
+(8) Exit" 
+    
+        read selection
+        
+        case "$selection" in
+
+            1)
+            bash foldermaker.sh
+            ;;
+            2)
+            bash foldercopier.sh
+            ;;
+            3)
+            bash setPassword.sh
+            ;;
+            4)
+            bash calculator.sh
+            ;;
+            5)
+            bash megafolder.sh
+            ;;          
+            6)
+            bash file_looper.sh
+            ;;
+            7)
+            bash internet_downloader.sh
+            ;;
+            8)
+            break
+            ;;
+            *)
+            echo "selection does not exist"
+            ;;
+        esac
+        
+    else 
+        echo "Access denied"; break
+
+    fi
+done
